9c8e4e320c0bcf6f39b7752f08de7e59405e03b1
ChiVinhVinh <vinhv4808@gmail.com>
2024-10-29 11:02:51 +0700

    MUI

--- src/ChoiGame/ChoiGame.tsx
+++ src/ChoiGame/ChoiGame.tsx
@@ -1,13 +1,35 @@
 import React, { useContext } from "react";
 import "./ChoiGame.css";
+import Stack from '@mui/material/Stack';
+import Button, { ButtonProps } from '@mui/material/Button';
 import Player from "./Player";
+import { styled } from '@mui/material/styles';
+import { green, grey } from '@mui/material/colors';
+import Box from '@mui/material/Box';
 import GameContextProvider, { GameContext } from "./GameContextProvider";
+const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText(grey[900]),
+  backgroundColor: grey[900],
+  '&:hover': {
+    backgroundColor: grey[500],
+  },
+}));
 const Choigame = () => {
   const { players, Deck, isRevealed, onShuffle, onDraw, onReveal, onReset } =
     useContext(GameContext);
   return (
-    <div className="game-container">
-      <div className="div1">
+    <Box sx={{ borderRadius: 1,
+      bgcolor: green[500], 
+      width: "50%",
+      height: "100vh",
+      margin: "0 auto", 
+      padding: 2,
+      flexDirection:"column",
+      display: "flex",
+      justifyContent: "center",
+      overflow:"hidden"}}>
+    <Stack direction="column" spacing={2} sx={{ flex: 1 }}>
+      <Stack direction="row" justifyContent="center">
         <Player
           playerData={players.player1}
           name="player1"
@@ -15,8 +37,11 @@ const Choigame = () => {
           status={players.player1.status}
           isRevealed={isRevealed}
         />
-      </div>
-      <div className="div2">
+      </Stack>
+      <Stack spacing={5}
+        direction="row"
+        justifyContent="center"
+        >
         <Player
           playerData={players.player4}
           name="player4"
@@ -24,25 +49,28 @@ const Choigame = () => {
           status={players.player4.status}
           isRevealed={isRevealed}
         />
-        <div className="Content">
+        <Stack spacing={3}
+            direction="column"
+          >
           <h2 className="h2">DECK CARD {Deck.deckCard}</h2>
-          <div className="btArea">
-            <button className="btShuffle" onClick={onShuffle}>
-              Shuffle
-            </button>
-            <button className="btDraw" onClick={onDraw}>
-              Draw
-            </button>
-            <button className="btReveal" onClick={onReveal}>
-              Reveal
-            </button>
-          </div>
-          <div className="bt">
-            <button className="btReset" onClick={onReset}>
-              Reset
-            </button>
-          </div>
-        </div>
+          <Stack direction="row" spacing={3}>
+           
+            <Button variant="contained" size="large" color="warning" onClick={onShuffle}>
+                Shuffle
+            </Button>
+            <Button variant="contained" size="large" color="success" onClick={onDraw}>
+                Draw
+            </Button>
+            <ColorButton variant="contained" size="large" color="inherit" onClick={onReveal}>
+            Reveal
+            </ColorButton>    
+          </Stack>
+          <Stack direction="row" justifyContent="center">
+            <Button variant="contained" size="large" color="error" onClick={onReset}>
+            Reset
+            </Button>
+            </Stack>
+        </Stack>
         <Player
           playerData={players.player2}
           name="player2"
@@ -50,8 +78,8 @@ const Choigame = () => {
           status={players.player2.status}
           isRevealed={isRevealed}
         />
-      </div>
-      <div className="div3">
+      </Stack>
+      <Stack direction="row" justifyContent="center">
         <Player
           playerData={players.player3}
           name="player3"
@@ -59,8 +87,9 @@ const Choigame = () => {
           status={players.player3.status}
           isRevealed={isRevealed}
         />
-      </div>
-    </div>
+      </Stack>
+    </Stack>
+    </Box>
   );
 };
 export default () => (
